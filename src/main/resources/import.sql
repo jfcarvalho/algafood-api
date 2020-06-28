a9453515ccb2d75d27003105ef0622369a6e360a
Jean Carvalho <jfcarvalho.profissional@mail.com>
2020-06-27 22:48:11 -0300

    Mapeando entidades

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into cozinha (nome) values ('Tailandesa');
+insert into cozinha (nome) values ('Chinesa');
+insert into cozinha (nome) values ('Japonesa');
+insert into cozinha (nome) values ('Brasileira');
