d038cb49cc912cc427d59e43bf9c5b4fec36bdef
Jean Carvalho <jfcarvalho.profissional@mail.com>
2020-06-27 23:45:42 -0300

    Métodos de persistencia

---
+++ src/main/java/AlterarCozinha.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.modelo.Cozinha;
+import com.algaworks.algafood.persistence.CadastroCozinha;
+
+public class AlterarCozinha {
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+					.web(WebApplicationType.NONE)
+					.run(args);
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		Cozinha cozinha = cadastroCozinha.buscar(1L);
+		cozinha.setNome("Espanhola");
+		
+		cozinha = cadastroCozinha.adicionar(cozinha);
+		System.out.println(cozinha.getId());
+		System.out.println(cozinha.getNome());
+		
+	}
+	
+
+}
---
+++ src/main/java/InclusaoCozinhaMain.java
@@ -0,0 +1,29 @@
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.modelo.Cozinha;
+import com.algaworks.algafood.persistence.CadastroCozinha;
+
+public class InclusaoCozinhaMain {
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+					.web(WebApplicationType.NONE)
+					.run(args);
+		
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Italiana");
+		
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Francesa");
+		
+		cadastroCozinha.adicionar(cozinha1);
+		cadastroCozinha.adicionar(cozinha2);
+		
+		
+		
+	}
+}
