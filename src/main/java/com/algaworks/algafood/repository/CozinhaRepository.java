ecdd641f679b2cfa93d4dfb2ea61d1e30da6038e
Jean Carvalho <jfcarvalho.profissional@mail.com>
2020-07-03 20:24:15 -0300

    Implementando mais funções do JPA

--- src/main/java/com/algaworks/algafood/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/repository/CozinhaRepository.java
@@ -1,19 +1,19 @@
 package com.algaworks.algafood.repository;
 
-//import java.util.List;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.modelo.Cozinha;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
-	//public List<Cozinha> listar();
-	//public List<Cozinha> consultarPorNomeSemSJPA(String nome);
-	//public Cozinha buscar(Long id);
-	//public Cozinha adicionar(Cozinha cozinha);
-	//public void remove(Long id);
 	
+	List<Cozinha> findTodasByNome(String nome);
+	List<Cozinha> findTodasByNomeContaining(String nome);
+	Optional<Cozinha> findByNome(String nome);
+	boolean existsByNome(String nome);
+	int countByCozinhaId(Long cozinha);
 }
--- src/main/java/com/algaworks/algafood/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/repository/RestauranteRepository.java
@@ -1,5 +1,9 @@
 package com.algaworks.algafood.repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,10 +12,21 @@ import com.algaworks.algafood.modelo.Restaurante;
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
+	/*
+	 * Procura taxas entre um determinado inteervalo
+	 */
 	
-	/*public List<Restaurante> listar();
-	public Restaurante buscar(Long id);
-	public Restaurante adicionar(Restaurante cozinha);
-	public void remove(Restaurante cozinha);
-	*/
+	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+	/*
+	 * Retornando o primeiro resultado que contem nome e o id da cozinha
+	 */
+	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+	/*
+	 * Retornando o primeiro resultado que contem nome 
+	 */
+	Optional<Restaurante> findFirstByNomeContaining(String nome);
+	/*
+	 * Retornando os 2 primeiros resultados
+	 */
+	List<Restaurante> findTop2ByNomeContaining(String nome);
 }
